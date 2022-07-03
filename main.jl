38358b1ce3aa7ed2bd3188982773673a55d61c90
tom <tomrottier@hotmail.com>
2022-07-03 21:36:10 +0200

    Chapter 6b A List of Hittable Obkects

--- main.jl
+++ main.jl
@@ -1,40 +1,81 @@
 using LinearAlgebra, Images, Term.Progress
 
+# abstract type for something a ray can hit
+abstract type AbstractHittable end
+
+# holds info on whether something hittable has been hit
+struct HitRecord{T<:AbstractFloat}
+    point::Vector{T} # location of hit
+    normal::Vector{T} # normal vector to hittable object at `point` - always point out from object
+    t::T # distance along ray to hit
+end
+
 struct Ray{T<:AbstractFloat}
     origin::Vector{T}
     direction::Vector{T}
 end
 
+# coordinates of a point `t` along ray
 point(r::Ray, t) = r.origin + r.direction * t
 
-# check if ray hits sphere
-function hit_sphere(centre, radius, r::Ray)
-    ac = r.origin - centre
-    a = r.direction ⋅ r.direction
-    halfb = r.direction ⋅ ac
-    c = ac ⋅ ac - radius^2
-    discriminant = halfb^2 - a * c
-
-    return discriminant < 0 ? -1 : (-halfb - √discriminant) / a
+struct Sphere{T<:AbstractFloat} <: AbstractHittable
+    origin::Vector{T}
+    radius::T
 end
 
+# location of intersection with ray
+function hit(sphere::Sphere, r::Ray, tmin, tmax)
+    # solve intersection equation
+    ac = r.origin - sphere.origin
+    a = r.direction ⋅ r.direction
+    halfb = r.direction ⋅ ac
+    c = ac ⋅ ac - sphere.radius^2
+    discriminant = halfb^2 - a * c
+
+    # return if no hit
+    discriminant < 0 && return false
+
+    # check if hit in bounds
+    t = (-halfb - √discriminant) / a
+    !(tmin ≤ t ≤ tmax) && return false
+
+    # return HitRecord otherwise
+    p = point(r, t)
+    normal = normalize(p - sphere.origin)
+    t = t
+
+    return HitRecord(p, normal, t)
+end
+
+# check hit in vector of hittables
+function hit(objs::Vector{T}, r::Ray, tmin, tmax) where {T<:AbstractHittable}
+    rec = 0.0
+    closest_so_far = tmax # will take smallest value along ray
+    hit_anything = false
+
+    # loop through hittable objects and return HitRecord for closest object
+    for obj in objs
+        temp_rec = hit(obj, r, tmin, closest_so_far)
+        if temp_rec != false && temp_rec.t < closest_so_far
+            closest_so_far = temp_rec.t
+            rec = temp_rec
+            hit_anything = true
+        end
+    end
+
+    return hit_anything ? rec : false
+end
+
+
 # determine colour of ray 
-function ray_colour(r::Ray)
-    # define sphere
-    sphere_centre = [0, 0, -1]
-    sphere_radius = 0.5
+function ray_colour(objs::Vector{T}, r::Ray) where {T<:AbstractHittable}
+    # check if ray hits anything
+    rec = hit(objs, r, 0.0, Inf)
 
-    # intersection point if exists
-    t = hit_sphere(sphere_centre, sphere_radius, r)
-
-    return if t > 0.0 # only want rays forward of camera
-        # normal vector to surface of sphere at intersection point 
-        normal = normalize(point(r, t) - sphere_centre)
-
-        # colour ray based on normal vector
-        RGB(0.5(normal .+ 1)...)
-
-    else # otherwise shade background normally
+    # return shaded if hit
+    return if rec != false
+        RGB(0.5(rec.normal + [1, 1, 1])...)
+    else # otherwise colour background normally
         _t = 0.5(normalize(r.direction)[2] + 1)
         RGB((1 - _t) * [1, 1, 1] + _t * [0.5, 0.7, 1.0]...)
     end
@@ -55,6 +96,12 @@ function main()
     origin = [0.0, 0.0, 0.0]
     viewport_origin = origin - [viewport_width / 2, 0, 0] - [0, viewport_height / 2, 0] - [0, 0, focal_length] # lower left corner
 
+    # world
+    sphere1 = Sphere([0.0, 0.0, -1.0], 0.5)
+    sphere2 = Sphere([0.0, -100.5, -1.0], 100.0)
+    world = [sphere1, sphere2]
+
+    # render
     @track for i in 1:image_width
         for j in 1:image_height
             # relative position of pixel in image
@@ -67,7 +114,7 @@ function main()
             r = Ray(origin, d)
 
             # determine colour of ray
-            c = ray_colour(r)
+            c = ray_colour(world, r)
 
             # reverse index to plot in same direction
             jj = reverse(1:image_height)[j]
